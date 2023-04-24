47871183da3a522a227ab92461daf4547e01bd00
salahmak <salah.mak@protonmail.com>
2023-04-24 16:59:04 +0100

    update factories and seeds to use USer instead of Student entity

--- src/db/factories/student.factory.ts
+++ src/db/factories/student.factory.ts
@@ -1,10 +1,8 @@
 import { setSeederFactory } from "typeorm-extension";
-import { User, UserType } from "../entities/UserEntity";
-import { Student } from "../entities/StudentEntity";
 import { Auth } from "../entities/AuthEntity";
+import { User, UserType } from "../entities/UserEntity";
 
-export default setSeederFactory(Student, async (faker) => {
-    const student = new Student();
+export default setSeederFactory(User, async (faker) => {
     const user = new User();
     const auth = new Auth();
 
@@ -14,13 +12,9 @@ export default setSeederFactory(Student, async (faker) => {
     user.email = faker.internet.email(user.name)
     user.phone_number = faker.phone.number("+213 ### ## ## ##")
     user.type = UserType.STUDENT
-
-
-    // initialize the student object
-    student.city = faker.address.city()
-    student.province = faker.address.state()
-    student.country = faker.address.country()
-    student.user = user
+    user.city = faker.address.city()
+    user.province = faker.address.state()
+    user.country = faker.address.country()
 
 
     // initialize the auth object
@@ -30,5 +24,5 @@ export default setSeederFactory(Student, async (faker) => {
     await auth.save()
 
 
-    return student;
+    return user;
 })
