bdf0959360d5a6d511dc5e448d8e76eaca939511
salahmak <salah.mak@protonmail.com>
2023-05-15 16:21:52 +0100

    implement require verification middleware

---
+++ src/middlewares/auth/requireVerification.middleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { config } from "../../configs/config";
+import { IAuthRequest } from "../../interfaces/requests.interface";
+import { User } from "../../db/entities/UserEntity";
+import { Auth } from "../../db/entities/Authentication/AuthEntity";
+import sendErrorResponse from "../../controllers/utils/makeErrorResponse.util";
+
+
+interface AuthTokenUser {
+    id: string;
+}
+
+/**
+ * 
+ * @description Should always run after requireAuth middleware since it depends on the authentication ID of the user
+ */
+export async function requireVerification(req: Request, res: Response, next: NextFunction) {
+    try {
+
+        const {id: authUserId} = (req as IAuthRequest).authUser
+
+        const userAuth = await Auth.findOne({
+            where: {userId: authUserId}
+        })
+
+        if(!userAuth) {
+            return sendErrorResponse("User doesn't exist", 400, res)
+        }
+
+
+        // checking the user's verification status
+        if(!userAuth.verified) {
+            return sendErrorResponse("User has to be verified to do this operation", 401, res);
+        }
+        
+        next();
+
+    } catch (error) {
+        res.status(500).json({
+            error: {
+                message: "An error has occurred while checking user verification status.",
+            },
+        });
+
+        return;
+    }
+}
